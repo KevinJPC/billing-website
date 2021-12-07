d73156d94abfc6a551b16c27010aaeb92e24c112
Karla Solórzano <Karli.02@hotmail.com>
2021-12-07 09:15:05 -0600

    Se agrego Paymethod

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import productReducer from '../features/product/productSlice';
+import paymethodReducer from '../features/payment-methods/paymethodSlice'
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     product: productReducer,
+    paymethod: paymethodReducer,
   },
 });
